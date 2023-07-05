736dda5dbc5127039d1d79eaabc472b15829d735
v0pe <dampblew01@gmail.com>
2023-07-04 23:24:27 -0600

    Setup the theme color and typography

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar (){
+    return (
+        <nav className="home__nav" style="transform: translate(0px, 0px); opacity: 1;">
+        <div className="home__nav__left"></div>
+        <div className="home__nav__right"></div>
+        </nav>
+    )
+}
