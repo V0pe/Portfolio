6818109da0d46b8a78474e74e8b5b52dc8b356c8
v0pe <dampblew01@gmail.com>
2023-07-09 08:52:31 -0600

    Add the navbar and hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+import './Hero.css';
+
+export default function Hero (){
+    return (
+    <section className="home__hero">
+      <div className="hero__title">
+        <h1 className="mobile" style= {{ transform: "translate(0px, 0px)" }}>
+           FULL <br/>
+            — STACK <br/>
+             DEVELOPER
+        </h1>
+      </div>
+    </section>)
+};
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,46 @@
+import './Navabr.css';
+
 export default function Navbar (){
     return (
-        <nav className="home__nav" style="transform: translate(0px, 0px); opacity: 1;">
-        <div className="home__nav__left"></div>
-        <div className="home__nav__right"></div>
+        <nav className="home__nav" style={{ transform: 'translate(0px, 0px)', opacity: '1' }} >
+        <div className="home__nav__left">
+            <div className="nav__name">
+                <span>
+                    Victor
+                    <br />
+                    Aderibigbe
+                </span>
+            </div>
+            <div className="hide-mobile nav__folio">
+                <span>
+                    Fullstack developer
+                    <br />
+                    Folio / 2022 - 2023
+                </span>
+            </div>
+        </div>
+        <div className="home__nav__right">
+            <div className="hide-desktop nav__folio">
+                <span>
+                    Fullstack developer
+                    <br />
+                    Folio / 2022 - 2023
+                </span>
+            </div>
+            <div className="nav__availability"> 
+              <span>Available for freelance 
+                <br/> 
+                work from March 2023
+              </span>
+            </div>
+            <button className="c-button contact-scroll nav__button" nav-anim="">
+                <span className="c-link">
+                    <span className="c-link__inner">
+                        <span className="c-link__animated"> contact </span>
+                     </span>
+                </span>
+            </button>
+        </div>
         </nav>
     )
 }
